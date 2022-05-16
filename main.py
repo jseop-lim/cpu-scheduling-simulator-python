6ac33fe31a7eb7c65dbd0fb1d17ac70ccf14ea31
jseop-lim <jseoplim@gmail.com>
2022-05-16 15:57:04 +0900

    Feat: run_scheduler()에 전략패턴 적용

--- main.py
+++ main.py
@@ -9,31 +9,35 @@ class Handler:
         else:
             raise ValueError('파일 경로를 입력하세요.')
 
-    def run_scheduler(self):
+    def run_scheduler(self, scheduler_class):
         """
         arrival_time list로 갖고 있으면서 수시로 검사
         현재 실행 중인 프로세스의 remain burst_time이 0이 될 때까지
         time_slice 지났는지 수시로 검사
         """
-        print('FCFS')
-        scheduler = FCFS(self.model)
+        scheduler = scheduler_class(self.model)
+        # gantt_data = {}
         scheduler.run()
-        print('\nRR')
-        scheduler = RR(self.model)
-        scheduler.run()
-        print('\npriority')
-        scheduler = Priority(self.model)
-        scheduler.run()
-        print('\npreemptive priority')
-        scheduler = PriorityPreemptive(self.model)
-        scheduler.run()
-        print('\npriority + RR')
-        scheduler = PriorityPreemptiveRR(self.model)
-        scheduler.run()
+
+        response_times = {process.pid: process.response for process in scheduler.terminated_queue}
+        turnaround_times = {process.pid: process.turnaround for process in scheduler.terminated_queue}
+        waiting_times = {process.pid: process.wait for process in scheduler.terminated_queue}
+
+        return response_times, turnaround_times, waiting_times
         # TODO 스케줄러 모듈이랑 이 함수랑 연결하기 - 입력? 필드?
 
     def main(self):
-        self.run_scheduler()
+        print('FCFS')
+        output = self.run_scheduler(FCFS)
+        print('\n'.join(map(str, output)))
+        print('\nRR')
+        self.run_scheduler(RR)
+        print('\npriority')
+        self.run_scheduler(Priority)
+        print('\npriority preemptive')
+        self.run_scheduler(PriorityPreemptive)
+        print('\npriority preemptive + RR')
+        self.run_scheduler(PriorityPreemptiveRR)
 
 
 if __name__ == '__main__':
